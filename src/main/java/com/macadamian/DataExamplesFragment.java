9c71d11f0d10b9dcfa78304cb78eac98bfa58d81
Don Kelly <karfai@gmail.com>
2013-10-02 12:26:26 +0400

    new sections

---
+++ src/main/java/com/macadamian/DataExamplesFragment.java
@@ -0,0 +1,8 @@
+package com.macadamian;
+
+public class DataExamplesFragment extends ExamplePreferenceFragment {
+    @Override
+    protected int getResourceId() {
+        return R.xml.data_examples;
+    }
+}
---
+++ src/main/java/com/macadamian/SystemExamplesFragment.java
@@ -0,0 +1,8 @@
+package com.macadamian;
+
+public class SystemExamplesFragment extends ExamplePreferenceFragment {
+    @Override
+    protected int getResourceId() {
+        return R.xml.system_examples;
+    }
+}

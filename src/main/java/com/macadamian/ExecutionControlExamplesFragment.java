58dec97e26f72b2e1b814da6a69460ae4f9b2dc5
Don Kelly <karfai@gmail.com>
2013-10-03 13:44:49 +0400

    intents activity

--- src/main/java/com/macadamian/ExecutionControlExamplesFragment.java
+++ src/main/java/com/macadamian/ExecutionControlExamplesFragment.java
@@ -1,6 +1,14 @@
 package com.macadamian;
 
+import android.os.Bundle;
+
 public class ExecutionControlExamplesFragment extends ExamplePreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        add("pref_intents", IntentsActivity.class);
+    }
+
     @Override
     protected int getResourceId() {
         return R.xml.execution_control_examples;
---
+++ src/main/java/com/macadamian/IntentsActivity.java
@@ -0,0 +1,12 @@
+package com.macadamian;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IntentsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intents);
+    }
+}

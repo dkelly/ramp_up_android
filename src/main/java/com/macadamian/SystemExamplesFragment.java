bf779353ca928689399b80c6a93bf4e1d5ef1b3f
Don Kelly <dkelly@macadamian.com>
2013-10-07 15:48:49 +0400

    pieces of in app billing example

--- src/main/java/com/macadamian/SystemExamplesFragment.java
+++ src/main/java/com/macadamian/SystemExamplesFragment.java
@@ -1,6 +1,14 @@
 package com.macadamian;
 
+import android.os.Bundle;
+
 public class SystemExamplesFragment extends ExamplePreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        add("pref_inapp_billing", InAppBillingActivity.class);
+    }
+
     @Override
     protected int getResourceId() {
         return R.xml.system_examples;

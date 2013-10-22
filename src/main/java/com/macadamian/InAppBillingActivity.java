dcbf3c421f1b364867a8f19d2c909f4126746d06
Don Kelly <dkelly@macadamian.com>
2013-10-22 23:46:40 +0400

    add unfinished changes

--- src/main/java/com/macadamian/InAppBillingActivity.java
+++ src/main/java/com/macadamian/InAppBillingActivity.java
@@ -13,16 +13,6 @@ public class InAppBillingActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBillingHelper = new IabHelper(
-            this,
-            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlL1EsvhmqS2wRpNmdQiFWSgvGXLSoP6c2upVrAHS1+96HYyNLxW8nVOp2gij5N1kUMwBKRuOBko7OHDrOIQiL6k2KNcYzeFe7L++8H5QZzjyDsNOO3vIBpk8FoPeQlq81ZzEbJsqr71BABgxV8gFzCJzXg6nhciEVXXgEWCS7b2MbA4oOX85drYYJTYLTOzVCb3tM2wcK+FNh06NlqGqHHoDSjSuBpkUmyASmMnY2etqUhKL36iztIdALQnswTYEJXV3nAxtDPAy7aov7Hhhvc0wea1lrg92ZtHj6/RBl7jwFcz6KXyGDDI17SSc5aWZMMqFFBGkS+7EdckI2gBbfwIDAQAB");
-        mBillingHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-                public void onIabSetupFinished(IabResult r) {
-                    if (!r.isSuccess()) {
-                        Log.e("rampup", "failed to setup IAB: " + r);
-                    }
-                }
-            });
     }
 
     @Override
